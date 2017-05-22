d53b00e3dfb2be30ade28b511eaf58ee8698f405
SreeAmbat <sreekumarambat@northwesternmutual.com>
2017-05-22 13:51:11 -0500

    Silly Story

---
+++ coding_challenges/sillystory/silly-stories.js
@@ -0,0 +1,38 @@
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+var textStr = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+var character = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+var location = ["the soup kitchen","Disneyland","the White House"];
+var event = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"];
+
+
+randomize.addEventListener('click', result);
+
+function result() {
+  alert("Hi")
+  var name = "Bob";
+  if(customName.value != '') {
+    var name = customName.value;
+    
+  }
+
+  if(document.getElementById("uk").checked) {
+    var weight = Math.round(300);
+    var temperature =  Math.round(94);
+    
+  }
+
+ textStr.replace(":insertx:", randomValueFromArray(character));
+ textStr.replace(":inserty:", randomValueFromArray(location));
+ textStr.replace(":insertz:", randomValueFromArray(event));
+ textStr.replace("Bob", name);
+ story.textContent = textStr;
+ story.style.visibility = 'visible';
+}
--- platform/JS Fundamentals/sandbox.js
+++ platform/JS Fundamentals/sandbox.js
@@ -13,18 +13,47 @@
 // (a)?console.log(true):console.log(false);
 
 
-var ninja = {
-  name:'Susanna',
-//   MEAN_belt:10,
-//   iOS_belt:10,
-//   python_belt:10,
-//   php_belt:9, // she hadn't mastered deploying yet!
-//   ruby_belt:9.75 // done in 1.5 hrs though!
-  belt:[10, 10, 10, 9, 9.75]
+// var ninja = {
+//   name:'Susanna',
+// //   MEAN_belt:10,
+// //   iOS_belt:10,
+// //   python_belt:10,
+// //   php_belt:9, // she hadn't mastered deploying yet!
+// //   ruby_belt:9.75 // done in 1.5 hrs though!
+//   belt:[10, 10, 10, 9, 9.75]
+// }
+// for (var key in ninja) {
+//   if (ninja.hasOwnProperty(key)) {
+//     console.log(key);
+//     console.log(ninja[key]);
+//   }
+// }
+
+
+// awesome();
+//  var awesome = function() {
+//   console.log("too good to be true");
+// }
+
+
+// awesome = ()=>{
+//   console.log("hi");
+// }
+
+// awesome();
+
+// var food = "Chicken";
+// function eat() {
+//   food = "half-chicken";
+//   console.log(food);
+//   var food = "gone";       // CAREFUL!
+//   console.log(food);
+// }
+// eat();
+// console.log(food);
+
+var new_word = "NEW!";
+function lastFunc() {
+  new_word = "old";
 }
-for (var key in ninja) {
-  if (ninja.hasOwnProperty(key)) {
-    console.log(key);
-    console.log(ninja[key]);
-  }
-}
+console.log(new_word);
