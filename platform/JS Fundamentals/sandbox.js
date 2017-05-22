c24295ddbaded24efe0514c87ce767b2d8349813
SreeAmbat <sreekumarambat@northwesternmutual.com>
2017-05-22 14:42:55 -0500

    Interpreter completed

--- platform/JS Fundamentals/sandbox.js
+++ platform/JS Fundamentals/sandbox.js
@@ -52,8 +52,44 @@
 // eat();
 // console.log(food);
 
-var new_word = "NEW!";
+// var hello = "interesting";
+// function example() {
+//   var hello = "hi!";
+//   console.log(hello);
+// }
+// example();
+// console.log(hello);
+
+
+// var new_word = "NEW!";
+// function lastFunc() {
+//   new_word = "old";
+// }
+// console.log(new_word);console.log(first_variable);
+
+
+// var first_variable;
+// function firstFunc() {
+//   first_variable = "Not anymore!!!";
+//   console.log(first_variable);
+// }
+// first_variable = "Yipee I was first!";
+// console.log(first_variable);
+
+// var food;
+// function eat() {
+//   food = "half-chicken";
+//   console.log(food);
+//   var food = "gone";       // CAREFUL!
+//   console.log(food);
+// }
+// food = "Chicken"
+// eat();
+// console.log(food);
+
+var new_word;
 function lastFunc() {
   new_word = "old";
 }
+new_word = "NEW!";
 console.log(new_word);
